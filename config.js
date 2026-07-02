f03403762278ea0bb55b5a6eb6cefdd39dbc9161
sutpower4 <67451489+sutpower4@users.noreply.github.com>
2026-07-02 10:33:44 +0700

    Update GAS_API_URL and DASHBOARD_URL in config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const TSNS_CONFIG = {
   LIFF_ID: "2010571742-kNZobt1U",
-  GAS_API_URL: "PUT_TSNS_IAM_WEB_APP_EXEC_URL_HERE",
-  DASHBOARD_URL: "PUT_TSNS_IAM_WEB_APP_EXEC_URL_HERE",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbzji0eaebhNUk7NJTDZ1fFN1gcWI6gew4lmrupheis5E3iPrJnUm1GpZSgaHMNtlcWu-g/exec",
+  DASHBOARD_URL: "https://script.google.com/macros/s/AKfycbzji0eaebhNUk7NJTDZ1fFN1gcWI6gew4lmrupheis5E3iPrJnUm1GpZSgaHMNtlcWu-g/exec",
   VERSION: "Sprint 6.1.3",
   ENV: "DEV"
 };
